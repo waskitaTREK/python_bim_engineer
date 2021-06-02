9aa1d61c3cf7bd1c3528a18965c16008ef443771
yessymarie <realitayessy@gmail.com>
2021-06-02 11:23:27 +0700

    loop

---
+++ example/4_loop.py
@@ -0,0 +1,9 @@
+# Loop
+## Loop item value
+fruits = ["mango","pineapple","orange","apple"]
+for x in fruits:
+    print(x)
+
+## Loop item using its index numbers
+for i in range(len(fruits)):
+    print("index " + str(i) + ": " + fruits[i])
