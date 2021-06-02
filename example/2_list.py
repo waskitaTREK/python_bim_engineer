8887f0e99c118e0efc06361b1764c6fc9a0a3ade
yessymarie <realitayessy@gmail.com>
2021-06-02 10:25:38 +0700

    List

---
+++ example/2_list.py
@@ -0,0 +1,32 @@
+# List type based on data
+list_string = ["Hello","Waskita","!"]
+list_boolean = [True,False,True]
+list_number = [1,2,3]
+
+list_mix = ["Ryan",25,True,180,"male"]
+
+# state the type of things
+print(type(list_string))
+
+# Access List Items
+print(list_string[1:])
+
+# Change Item Value's using index number
+list_mix[2] = False
+##  Insert Item Value based on specific index
+list_mix.insert(5,"Jakarta")
+## Insert item at the end of the list
+list_mix.append("August 15th, 1995")
+
+print(list_mix)
+
+## Append list 1 to list 2
+list1 = ["apple", "banana", "cherry"]
+list2 = ["mango", "pineapple", "papaya"]
+list1.extend(list2)
+print(list1)
+
+#Remove item from list
+## Remove item from list using item 
+list1.remove("banana")
+print(list1)
