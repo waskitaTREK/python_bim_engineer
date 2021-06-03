baebc0d2168760adc6718dd7c3a29c90ce75227e
fahmiduldul <afahmi13@live.com>
2021-06-03 14:41:19 +0700

    feat: add faster fibbonaci

--- example/8_recursion.py
+++ example/8_recursion.py
@@ -1,17 +1,37 @@
 # Python program to display the Fibonacci sequence
 
+# usual fibbonaci
 def fibonacci(n):
-   if n <= 1:
-       return n
-   else:
-       return(fibonacci(n-1) + fibonacci(n-2))
+    # check if number not negative
+    if n < 0:
+        raise Exception("Please insert positive integer")
+    
+    # if number is 0 or 1, return the number
+    if n <= 1:
+        return n
+
+    # do the recursion
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# faster fibbonaci
+def faster_fib(n, mem={}):
+    # check if number not negative
+    if n < 0:
+        raise Exception("Please insert positive integer")
+    
+    # if number is 0 or 1, return the number
+    if n <= 1:
+        return n
+    
+    # add calculated number to memory
+    if n not in mem.keys():
+        mem[n] = faster_fib(n-1, mem) + faster_fib(n-2, mem)
+
+    # return the number from memory
+    return mem[n]
 
 n = int(input("Enter a number: "))
 
-# check if the number of terms is valid
-if n <= 0:
-   print("Plese enter a positive integer")
-else:
-   print("Fibonacci sequence:")
-   for i in range(n):
-       print(fibonacci(i))
+print(fibonacci(n))
+
+print(faster_fib(n))
