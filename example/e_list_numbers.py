cf9c9d0ced5f5f48a32fb23207131c36a32443fa
fahmiduldul <afahmi13@live.com>
2021-06-07 10:13:33 +0700

    feat: add numebr to list wo convert to string

--- example/e_list_numbers.py
+++ example/e_list_numbers.py
@@ -1,12 +1,29 @@
-
 def return_of_list_numbers(number):
     list = []
     for i in range(len(number)):
         list.append(int(number[i]))
     return list
 
-input = input("Enter the number: ")
+def number_to_list_wo_string(number):
+    numbers = []
 
-list_of_numbers = return_of_list_numbers(input)
+    # find the max value of 10s
+    n = 0
+    while number / 10**n > 10.0:
+        n += 1
+    
+    # add number to numbers
+    for i in range(n,-1,-1):
+        temp = number // 10**i
+        numbers.append(temp)
+        number = number - temp*10**i
+    
+    return numbers
+
+
+number = int(input("Enter the number: "))
+
+# list_of_numbers = return_of_list_numbers(number)
+list_of_numbers = number_to_list_wo_string(number)
 
 print(list_of_numbers)
