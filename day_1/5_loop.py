aa130a7845a7e278c825845c2aff36af94992721
fahmiduldul <afahmi13@live.com>
2021-06-09 10:17:22 +0700

    add: loop

--- day_1/5_loop.py
+++ day_1/5_loop.py
@@ -1,4 +1,22 @@
 number = 0
 
-while number < 5:
-    
+while number <= 5:
+    # print(number)
+
+    # number = number + 1
+    number += 1
+
+persons = ["Taufiq", "Naufal", "Syamsul", "Yessy", "Ervan"]
+
+index = 0
+while index < len(persons):
+    # print(persons[index])
+
+    index += 1
+
+for name in persons:
+    # print(name)
+    pass
+
+for number in range(5,14):
+    print(number)
