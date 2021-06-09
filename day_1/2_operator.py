e30277eacf23bd148f995393a9e31bf64a6cac44
fahmiduldul <afahmi13@live.com>
2021-06-09 10:03:38 +0700

    feat: add comparator greater than etc

--- day_1/2_operator.py
+++ day_1/2_operator.py
@@ -46,4 +46,9 @@ print(num1 != num2)
 print(num2 != num3)
 
 isEqual = num2 == num3
-print(isEqual)
+# print(isEqual)
+
+print(num1 < num2)
+print(num1 > num2)
+print(num3 <= num2)
+print(num3 < num2)
