f9e5e2580b8696a1254139dfc18a0468365481a4
fahmiduldul <afahmi13@live.com>
2021-06-09 15:58:09 +0700

    add: recursion file

---
+++ day_1/12_recursion.py
@@ -0,0 +1,43 @@
+def printNumbersUntil0(number):
+    print(number)
+
+    # number = number - 1
+    if number > 0:
+        printNumbersUntil0(number - 1)
+
+# printNumbersUntil0(6)
+
+def fibbonaci(index):
+    # check if index is negative value
+    if index < 0:
+        print("please insert non negative integer")
+        return
+    
+    # check if index is 0 or 1
+    if index in [0,1]:
+        return index
+    
+    # call recursion
+    else:
+        return fibbonaci(index - 1) + fibbonaci(index - 2)
+
+def faster_fib(index, mem={}):
+    if index < 0:
+        print("please insert non negative integer")
+        raise Exception("err")
+    
+    # check if index is 0 or 1
+    if index in [0,1]:
+        return index
+    
+    elif index not in mem.keys():
+        mem[index] = faster_fib(index - 1,mem) + faster_fib(index - 2,mem)
+    
+    return mem[index]
+
+# print(faster_fib(998))
+
+numbers = [4,6,4,25,6,7,8]
+
+# print(dicti.keys())
+# print(dicti.values())
